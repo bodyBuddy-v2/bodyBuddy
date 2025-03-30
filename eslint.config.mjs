f01307f46ec33cf1df22afdcd5f0e4ccd1a39588
mei.zy <alswlkku@gmail.com>
2025-03-31 01:32:04 +0900

    fix: eslint 설정 업데이트

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
 import { FlatCompat } from "@eslint/eslintrc";
 import pluginQuery from "@tanstack/eslint-plugin-query";
+import fsdPlugin from "eslint-plugin-fsd-lint";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 import simpleImportSort from "eslint-plugin-simple-import-sort";
 import unusedImports from "eslint-plugin-unused-imports";
@@ -21,10 +22,17 @@ const eslintConfig = [
     plugins: {
       "unused-imports": unusedImports,
       "simple-import-sort": simpleImportSort,
+      "fsd-plugin": fsdPlugin,
     },
     rules: {
+      "fsd-plugin/forbidden-imports": "error",
+      "fsd-plugin/no-relative-imports": "error",
+      "fsd-plugin/no-cross-slice-dependency": "error",
+      "fsd-plugin/no-ui-in-business-logic": "error",
+      "fsd-plugin/no-global-store-imports": "error",
+      "fsd-plugin/ordered-imports": "warn",
       "simple-import-sort/imports": "error",
-      "no-unused-vars": "off", // or "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "warn", // or "@typescript-eslint/no-unused-vars": "off",
       "unused-imports/no-unused-imports": "error",
       "unused-imports/no-unused-vars": [
         "warn",

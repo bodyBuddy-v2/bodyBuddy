6fff758dc7b045a483045a73276b7d4a8db69a9e
Park sian <52738906+sians5544@users.noreply.github.com>
2025-03-31 23:50:55 +0900

    style : 운동목적, 관심 종목 checkbox 너비 스타일 변경

--- src/widgets/userSignUp/ui/Step3.tsx
+++ src/widgets/userSignUp/ui/Step3.tsx
@@ -1,4 +1,4 @@
-import { Button, Checkbox, Col, Form, Row, Space, Typography } from "antd";
+import { Button, Checkbox, Col, Form, Row, Typography } from "antd";
 import { Controller, useFormContext } from "react-hook-form";
 
 import { checkAllDirty, checkAllErrors } from "@/features";
@@ -20,62 +20,73 @@ const UserSignUpStep3 = () => {
 
   return (
     <>
-      <Space align="start" direction="vertical" style={{ width: "100%" }}>
-        <Controller
-          name={UserFormKey.GOALS}
-          control={control}
-          render={({ field }) => (
-            <Form.Item
-              validateStatus={errors[UserFormKey.GOALS] ? "error" : ""}
-              help={errors[UserFormKey.GOALS] ? errors[UserFormKey.GOALS]?.message : ""}
+      <Controller
+        name={UserFormKey.GOALS}
+        control={control}
+        render={({ field }) => (
+          <Form.Item
+            style={{ width: "100%" }}
+            validateStatus={errors[UserFormKey.GOALS] ? "error" : ""}
+            help={errors[UserFormKey.GOALS] ? errors[UserFormKey.GOALS]?.message : ""}
+          >
+            <Typography.Text strong>운동 목적</Typography.Text>
+            <Typography style={{ fontSize: "12px", color: "#7D7D7D", paddingBottom: "10px" }}>
+              최대 2개가 선택 가능합니다.
+            </Typography>
+            <Checkbox.Group
+              {...field}
+              style={{
+                background: "rgba(0,0,0,0.1)",
+                minHeight: "100px",
+                width: "100%",
+                paddingLeft: "15px",
+                borderRadius: "2px",
+              }}
             >
-              <Typography.Text strong>운동 목적</Typography.Text>
-              <Typography style={{ fontSize: "12px", color: "#7D7D7D", paddingBottom: "10px" }}>
-                최대 2개가 선택 가능합니다.
-              </Typography>
-              <Checkbox.Group
-                {...field}
-                style={{ background: "rgba(0,0,0,0.1)", minHeight: "100px", paddingLeft: "15px", borderRadius: "2px" }}
-              >
-                <Row justify="space-around" align="middle">
-                  {fieldList.map(option => (
-                    <Col span={12} key={option.value}>
-                      <Checkbox value={option.value}>{option.label}</Checkbox>
-                    </Col>
-                  ))}
-                </Row>
-              </Checkbox.Group>
-            </Form.Item>
-          )}
-        />
-        <Controller
-          name={UserFormKey.CATEGORY}
-          control={control}
-          render={({ field }) => (
-            <Form.Item
-              validateStatus={errors[UserFormKey.CATEGORY] ? "error" : ""}
-              help={errors[UserFormKey.CATEGORY] ? errors[UserFormKey.CATEGORY]?.message : ""}
+              <Row justify="space-around" align="middle">
+                {fieldList.map(option => (
+                  <Col span={12} key={option.value}>
+                    <Checkbox value={option.value}>{option.label}</Checkbox>
+                  </Col>
+                ))}
+              </Row>
+            </Checkbox.Group>
+          </Form.Item>
+        )}
+      />
+      <Controller
+        name={UserFormKey.CATEGORY}
+        control={control}
+        render={({ field }) => (
+          <Form.Item
+            validateStatus={errors[UserFormKey.CATEGORY] ? "error" : ""}
+            help={errors[UserFormKey.CATEGORY] ? errors[UserFormKey.CATEGORY]?.message : ""}
+          >
+            <Typography.Text strong>관심 종목</Typography.Text>
+            <Typography style={{ fontSize: "12px", color: "#7D7D7D", paddingBottom: "10px" }}>
+              최대 3개가 선택 가능합니다.
+            </Typography>
+            <Checkbox.Group
+              {...field}
+              style={{
+                background: "rgba(0,0,0,0.1)",
+                minHeight: "100px",
+                width: "100%",
+                paddingLeft: "15px",
+                borderRadius: "2px",
+              }}
             >
-              <Typography.Text strong>관심 종목</Typography.Text>
-              <Typography style={{ fontSize: "12px", color: "#7D7D7D", paddingBottom: "10px" }}>
-                최대 3개가 선택 가능합니다.
-              </Typography>
-              <Checkbox.Group
-                {...field}
-                style={{ background: "rgba(0,0,0,0.1)", minHeight: "100px", paddingLeft: "15px", borderRadius: "2px" }}
-              >
-                <Row justify="space-around" align="middle">
-                  {exerciseList.map(option => (
-                    <Col span={12} key={option.value}>
-                      <Checkbox value={option.value}>{option.label}</Checkbox>
-                    </Col>
-                  ))}
-                </Row>
-              </Checkbox.Group>
-            </Form.Item>
-          )}
-        />
-      </Space>
+              <Row justify="space-around" align="middle">
+                {exerciseList.map(option => (
+                  <Col span={12} key={option.value}>
+                    <Checkbox value={option.value}>{option.label}</Checkbox>
+                  </Col>
+                ))}
+              </Row>
+            </Checkbox.Group>
+          </Form.Item>
+        )}
+      />
       <Form.Item>
         <Button
           style={{ paddingTop: "auto", width: "100%" }}

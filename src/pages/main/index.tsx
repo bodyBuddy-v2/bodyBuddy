a7a36a0161d5a6a99f1434bfc3f8c13ebeeab961
Park sian <52738906+sians5544@users.noreply.github.com>
2025-04-07 22:18:35 +0900

    refactor: 상세 옵션 모달 컴포넌트 이관

---
+++ src/pages/main/index.tsx
@@ -0,0 +1,79 @@
+import { blue } from "@ant-design/colors";
+import { Flex, Typography } from "antd";
+import { useState } from "react";
+
+import { TrainerProfileType } from "@/entities/trainer";
+
+import { TrainerProfile } from "./ui";
+
+const Main = () => {
+  const [TestList, setTestList] = useState<TrainerProfileType[]>([
+    {
+      images: [
+        {
+          uid: "-1",
+          name: "image.png",
+          status: "done",
+          url: "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
+        },
+        {
+          uid: "-2",
+          name: "image.png",
+          status: "done",
+          url: "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
+        },
+      ],
+      name: "황재민",
+      profile: "매우 반복적인 식단, 지겨우셨죠?",
+      category: "PT",
+      cost: "15,000",
+    },
+    {
+      images: [
+        {
+          uid: "-1",
+          name: "image.png",
+          status: "done",
+          url: "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
+        },
+        {
+          uid: "-2",
+          name: "image.png",
+          status: "done",
+          url: "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
+        },
+      ],
+      name: "황세민",
+      profile: "매우 반복적인 식단, 그게 답이다",
+      category: "PT",
+      cost: "25,000",
+    },
+  ]);
+
+  return (
+    <>
+      <Flex vertical style={{ height: "100%", flex: "auto" }}>
+        <Typography.Title level={2}>
+          <Typography.Text strong style={{ color: blue[5], fontSize: "30px" }}>
+            손흥민
+          </Typography.Text>
+          님 <br />
+          반갑습니다.
+        </Typography.Title>
+
+        {TestList.map((list, index) => (
+          <TrainerProfile
+            key={index}
+            images={list.images}
+            name={list.name}
+            profile={list.profile}
+            category={list.category}
+            cost={list.cost}
+          />
+        ))}
+      </Flex>
+    </>
+  );
+};
+
+export default Main;

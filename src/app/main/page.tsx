bb894f7fc9bde4ab0822bcf9f4fd1bf6af32d9a7
Park sian <52738906+sians5544@users.noreply.github.com>
2025-04-08 18:32:44 +0900

    fix : build 오류 수정

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,5 +1,5 @@
 // app/main/page.tsx
-import { Main } from "@/pages/Main"; // 또는 "@/pages/Main/index"
+import { Main } from "@/pages/Main/MainPage"; // 또는 "@/pages/Main/index"
 
 export default function Page() {
   return <Main />;

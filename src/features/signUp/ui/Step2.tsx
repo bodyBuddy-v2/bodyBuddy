9ea083d5a667f52a7b1d2e9685bc2c4038d43678
Park sian <52738906+sians5544@users.noreply.github.com>
2025-12-02 00:47:56 +0900

    fix : signUp feature 폴더로 변경

--- src/features/signUp/ui/Step2.tsx
+++ src/features/signUp/ui/Step2.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Button, Flex, Form, Input, Radio, Select, Typography } from "antd";
 import { useState } from "react";
 import { Controller, useFormContext } from "react-hook-form";
